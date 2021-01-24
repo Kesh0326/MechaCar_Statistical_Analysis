5aa9122b3488eb5ae0c39fd37d0bd5b0cae91146
Kesh0326 <70290766+Kesh0326@users.noreply.github.com>
2021-01-24 13:02:32 -0800

    Add files via upload

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -1,1 +1,4 @@
 library(dplyr)
+mechacar_mpg <- read.csv('MechaCar_mpg.csv', check.names = F, stringsAsFactors = F) #read in dataset
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mechacar_mpg) #generate multiple linear regression model
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mechacar_mpg)) #generate summary statistics
