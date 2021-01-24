24c6703e6d10e773f50f1b3bf789fdbccb7d8016
Kesh0326 <70290766+Kesh0326@users.noreply.github.com>
2021-01-24 15:47:57 -0800

    Add files via upload

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -5,3 +5,10 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 suspension_coil <- read.csv('Suspension_Coil.csv', check.names = F, stringsAsFactors = F) #read in Suspension Coil dataset
 total_summary <- suspension_coil %>% summarize(Mean = mean(suspension_coil$PSI), Median = median(suspension_coil$PSI), Variance = var(suspension_coil$PSI), SD = sd(suspension_coil$PSI), .groups = 'keep') #create summary table with multiple columns
 lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep') #create summary table to group PSI column data by lots
+t.test(suspension_coil$PSI, mu = 1500) #compare overall PSI vs. mean of 1500
+PSI_lot1 <- suspension_coil %>% subset(Manufacturing_Lot=="Lot1") #select only datapoints where manufacturing lot is Lot1
+PSI_lot2 <- suspension_coil %>% subset(Manufacturing_Lot=="Lot2") #select only datapoints where manufacturing lot is Lot2
+PSI_lot3 <- suspension_coil %>% subset(Manufacturing_Lot=="Lot3") #select only datapoints where manufacturing lot is Lot3
+t.test(PSI_lot1$PSI, mu = 1500) #compare Lot1 PSI vs. mean of 1500
+t.test(PSI_lot2$PSI, mu = 1500) #compare Lot2 PSI vs. mean of 1500
+t.test(PSI_lot3$PSI, mu = 1500) #compare Lot3 PSI vs. mean of 1500
